616f1871969ea2da9e3ac4da238a3517865fab55
kudojurina <167832586+kudojurina@users.noreply.github.com>
2024-10-22 09:28:52 +0900

    Update ex6-1.js

--- ex6-1.js
+++ ex6-1.js
@@ -2,3 +2,8 @@ function sayhello61(){
   alert('Hello,Function!');}
 function sayhello61(){
   alert('saygoodbye,Goodbye!');}
+var who65='world';
+function sayhello65(){alert('hello,'+who65;'!');}
+hunction taro65(){who65='太郎';
+                  hunction hanako65(){who65=花子';}
+                    
