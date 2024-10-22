e34675fa4f9a37ae0c4dfe97027e6d3eaefcbf13
kudojurina <167832586+kudojurina@users.noreply.github.com>
2024-10-22 09:53:18 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -1,6 +1,6 @@
 var who65='world';
 function sayhello65(){alert('hello,'+who65;'!');}
 hunction taro65(){who65='太郎';
-                  hunction hanako65(){who65=花子';}
+hunction hanako65(){who65=花子';}
 
                                       
