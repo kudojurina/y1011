ed20e2a297bd5ac16f45e3f9e926829bb59a91a6
kudojurina <167832586+kudojurina@users.noreply.github.com>
2024-11-12 09:55:20 +0900

    Update ex.6-7.js

--- ex.6-7.js
+++ ex.6-7.js
@@ -1,4 +1,4 @@
 var who67='world';
 function sayhello67(){alert('Hello,'+who67+'!');}
-function someone(x){who67=x;}
-function someone(x){who67=x;}
+function someone(taro67){who67=x;}
+function someone(hanako67){who67=x;}
