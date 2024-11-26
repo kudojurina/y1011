54c193952af294058f63787e8a85a4d4bbff93a7
kudojurina <167832586+kudojurina@users.noreply.github.com>
2024-11-26 22:38:28 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,3 +1,3 @@
-function imgchange1(){document.getElementById('mark').src='./image/naku.png';}
-function imgchange2(){document.getElementById('mark').src='./image/nikoniko.png';}
+function imgchange1(){document.getElementById('mark').src='./image/hiraku.png';}
+function imgchange2(){document.getElementById('mark').src='./image/yorime.png';}
 
