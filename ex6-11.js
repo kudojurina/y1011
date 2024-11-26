aa9ee182c80dc67c8be686de4510c949d1b4b24a
kudojurina <167832586+kudojurina@users.noreply.github.com>
2024-11-26 22:28:29 +0900

    Create ex6-11.js

---
+++ ex6-11.js
@@ -0,0 +1,6 @@
+function imgchange3() {
+    document.getElementById('logo').src = './image/jokoma.jpg';
+}
+function imgchange4() {
+    document.getElementById('logo').src = './image/josai.jpg';
+}
