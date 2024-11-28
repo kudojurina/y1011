8f72285c6c3f9b1cade1bdf1ab3123adaa5e3d32
kudojurina <167832586+kudojurina@users.noreply.github.com>
2024-11-28 10:35:21 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,1 +1,3 @@
-function gazo688() {document.getElementById('who688').innerHTML = '<img src="./image/hiraku.png" alt="オン">';}
+function gazo688() {
+    document.getElementById('who688').innerHTML = '<img src="./image/naku.png" alt="オン">';
+}
