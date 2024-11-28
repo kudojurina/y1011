a22fb40b01d582fca6fad5997be3adb00138d364
kudojurina <167832586+kudojurina@users.noreply.github.com>
2024-11-28 10:38:03 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,2 +1,6 @@
-function imgchange3() {document.getElementById('logo').src = './image/jokoma.jpg';}
-function imgchange4() {document.getElementById('logo').src = './image/josai.jpg';}
+function imgchange3() {
+    document.getElementById('logo').src = './image/jokoma.jpg';
+}
+function imgchange4() {
+    document.getElementById('logo').src = './image/josai.jpg';
+}
